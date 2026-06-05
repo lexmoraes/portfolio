253fa19d0dfb1e93439a5c9e519471bda5af6e63
Alex <lexfisica@gmail.com>
2026-06-05 16:09:51 -0400

    FEAT (#1382) - dark mode

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, afterNextRender } from '@angular/core';
+import { Component, signal, afterNextRender, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,6 +6,7 @@ import { Project } from '../../core/models/project';
 import { MOCK_PROJECTS } from '../../core/mocks/projects-mocks';
 import { ProjectListComponent } from '../projects/pages/project-list/project-list.component';
 import { AboutComponent } from '../about/about.component';
+import { ThemeService } from '../../shared/services/theme.service';
 
 declare const AOS: any;
 
@@ -19,6 +20,8 @@ declare const AOS: any;
 export class HomeComponent {
   projects = signal<Project[]>(MOCK_PROJECTS);
   developerName = signal<string>('Alexsandro Moraes');
+
+  themeService = inject(ThemeService);
 
   constructor() {
     afterNextRender(() => {
--- src/layouts/main-layout/main-layout.component.ts
+++ src/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common'; // 💡 Necessário para proteger o SSR
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { ThemeService } from '../../shared/services/theme.service';
+
+type ThemeMode = 'light' | 'dark' | 'matrix';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [ MatButtonModule, RouterOutlet ],
+  imports: [MatButtonModule, RouterOutlet, MatIcon, MatMenu, MatMenuTrigger],
   templateUrl: './main-layout.component.html'
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  themeService = inject(ThemeService);
+  currentTheme = signal<ThemeMode>('light');
+  private platformId = inject(PLATFORM_ID);
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedTheme = localStorage.getItem('user-portfolio-theme') as ThemeMode;
+      if (savedTheme) {
+        this.changeTheme(savedTheme);
+      }
+    }
+  }
+
+  changeTheme(theme: ThemeMode): void {
+    this.currentTheme.set(theme);
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('user-portfolio-theme', theme);
+
+      const root = document.documentElement;
+
+      root.classList.remove('light', 'dark', 'matrix');
+
+      root.classList.add(theme);
+    }
+  }
+}
